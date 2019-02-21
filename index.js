e819c0c26870af9ba519da13c50e9e83635d9368
Merline Belizaire <merline@noukod.com>
2019-02-21 16:58:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const currentUser = 'Grace Hopper';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+shortGreeting
+  contains the first initial of the name stored in the 'currentUser' variable
+    AssertionError: expected 'Welcome, Grace Hopper' to not contain 'race Hopper'
